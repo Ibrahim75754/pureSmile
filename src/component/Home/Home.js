9848e661b1c6379fc2233a4f95d29de0b4793bbc
Ibrahim Hossain <ih75754@gmail.com>
2021-10-19 19:03:01 +0600

    home page complete

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -4,6 +4,7 @@ import Header from '../Shared/Header';
 import Banner from './Banner';
 import Partners from './Partners';
 import Services from './Services';
+import WhyChoose from './WhyChoose';
 
 const Home = () => {
     return (
@@ -11,6 +12,7 @@ const Home = () => {
             <Header></Header>
             <Banner></Banner>
             <Services></Services>
+            <WhyChoose></WhyChoose>
             <Partners></Partners>
             <Footer></Footer>
         </div>
